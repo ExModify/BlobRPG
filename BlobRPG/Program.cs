acf4c1aefa8a1627072518930d2ad4fa284d399a
ExModify <exmodify@gmail.com>
2021-02-17 18:17:42 +0100

    Add basic window

--- BlobRPG/Program.cs
+++ BlobRPG/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BlobRPG.Window;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Runtime.InteropServices;
 
 namespace BlobRPG
 {
@@ -6,7 +10,26 @@ namespace BlobRPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameWindowSettings gameWindowSettings = new GameWindowSettings()
+            {
+                RenderFrequency = 240,
+                UpdateFrequency = 240
+            };
+            NativeWindowSettings nativeWindowSettings = new NativeWindowSettings()
+            {
+                API = ContextAPI.OpenGL,
+                APIVersion = new Version(3, 2),
+                AutoLoadBindings = true,
+                Flags = ContextFlags.Default,
+                Profile = ContextProfile.Core,
+                Size = new OpenTK.Mathematics.Vector2i(1280, 720),
+                Title = "BlobRPG"
+            };
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                nativeWindowSettings.Flags |= ContextFlags.ForwardCompatible;
+
+            using Game game = new Game(gameWindowSettings, nativeWindowSettings);
+            game.Run();
         }
     }
 }
---
+++ BlobRPG/Window/Game.cs
@@ -0,0 +1,30 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlobRPG.Window
+{
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) { }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+        }
+    }
+}
