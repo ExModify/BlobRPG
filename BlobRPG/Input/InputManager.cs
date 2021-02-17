9061ec4cd3007188f41654cbfc4111a345083878
ExModify <exmodify@gmail.com>
2021-02-17 19:52:28 +0100

    Add basic input manager

---
+++ BlobRPG/Input/InputManager.cs
@@ -0,0 +1,90 @@
+﻿using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlobRPG.Input
+{
+    // OpenTK's KeyPressed function malfunctions
+    // Probably I'm just unaware how to configure it properly
+    public static class InputManager
+    {
+        static List<Keys> keysDown;
+        static KeyboardState keyboardState;
+        static MouseState mouseState;
+
+        static float lastX = 0, lastY = 0, lastScroll = 0, scroll = 0;
+
+        public static float X { get; private set; } = 0;
+        public static float Y { get; private set; } = 0;
+        public static float XDelta { get; private set; } = 0;
+        public static float YDelta { get; private set; } = 0;
+        public static float ScrollDelta { get; private set; } = 0;
+
+        public static bool MouseLocked { get; private set; } = true;
+
+        public static void Init(GameWindow window)
+        {
+            keysDown = new List<Keys>();
+            window.CursorGrabbed = true;
+        }
+
+        public static void ToggleMouse(GameWindow window)
+        {
+            window.CursorGrabbed = !window.CursorGrabbed;
+
+            if (!window.CursorGrabbed)
+                window.CursorVisible = true;
+
+            MouseLocked = !MouseLocked;
+        }
+
+        public static void Update(GameWindow window)
+        {
+            keyboardState = window.KeyboardState;
+            mouseState = window.MouseState;
+
+            for (int i = 0; i < keysDown.Count;)
+            {
+                if (!keyboardState.IsKeyDown(keysDown[i]))
+                    keysDown.RemoveAt(i);
+                else i++;
+            }
+
+            lastX = X;
+            lastY = Y;
+            lastScroll = scroll;
+
+            X = mouseState.X;
+            Y = mouseState.Y;
+            scroll = mouseState.Scroll.Y;
+
+            XDelta = X - lastX;
+            YDelta = Y - lastY;
+            ScrollDelta = scroll - lastScroll;
+        }
+        public static bool IsKeyDown(Keys key)
+        {
+            bool down = keyboardState.IsKeyDown(key);
+
+            if (down && !keysDown.Contains(key))
+                keysDown.Add(key);
+            return down;
+        }
+        public static bool IsKeyUp(Keys key)
+        {
+            bool down = keyboardState.IsKeyDown(key);
+            return !down;
+        }
+        public static bool IsKeyPressed(Keys key)
+        {
+            bool down = keyboardState.IsKeyDown(key);
+            bool wasPressed = keysDown.Contains(key);
+
+            if (down && !wasPressed)
+                keysDown.Add(key);
+            return down && !wasPressed;
+        }
+    }
+}
--- BlobRPG/Window/Game.cs
+++ BlobRPG/Window/Game.cs
@@ -4,21 +4,37 @@ using OpenTK.Graphics.OpenGL;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BlobRPG.Input;
 
 namespace BlobRPG.Window
 {
     public class Game : GameWindow
     {
+
         public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) { }
 
         protected override void OnLoad()
         {
             base.OnLoad();
+
             GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
+            InputManager.Init(this);
         }
 
         protected override void OnUpdateFrame(FrameEventArgs args)
         {
+            InputManager.Update(this);
+
+            if (InputManager.IsKeyPressed(OpenTK.Windowing.GraphicsLibraryFramework.Keys.Escape))
+            {
+                Close();
+            }
+            if (InputManager.IsKeyPressed(OpenTK.Windowing.GraphicsLibraryFramework.Keys.LeftControl))
+            {
+                InputManager.ToggleMouse(this);
+            }
+            
+            
             base.OnUpdateFrame(args);
         }
 
